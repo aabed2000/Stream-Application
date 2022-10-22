27f8a2343cc14170276f54703d95f416f898a0ef
Abed Toumeh <a.toumeh2000@gmail.com>
2022-10-22 13:01:36 +0300

    Final Face For that Project

---
+++ client/src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
+import "../style/App.css";
+
+// We use exact to give only page one , beacause if we didnt put it , then it will show page one and page 2 in pageTwo, because it will see the slash in the begainning and then it will see the pageTwo , then it will show both of it together .
+const App = () => {
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
